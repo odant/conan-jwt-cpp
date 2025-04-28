0dfd4c03eb596db2aa43e8a59e25f0dd57f186f7
Arkady Yudintsev <udincev@odant.ru>
2025-04-28 13:40:54 +0300

    Adapted to Conan v2

--- conanfile.py
+++ conanfile.py
@@ -1,8 +1,4 @@
-from conan import ConanFile
-from conan.tools.scm import Version
-from conan.tools.files import get, copy, apply_conandata_patches, export_conandata_patches
-from conan.tools.layout import basic_layout
-from conan.tools.cmake import CMakeDeps
+from conan import ConanFile, tools
 import os
 
 required_conan_version = ">=1.52.0"
@@ -18,42 +14,54 @@ class JwtCppConan(ConanFile):
     package_type = "header-library"
     settings = "os", "arch", "compiler", "build_type"
 
-    options = { "with_picojson": [True, False] }
-    default_options = { "with_picojson" : False }
-    
+    options = { 
+        "with_picojson": [True, False]
+    }
+    default_options = { 
+        "with_picojson" : False
+    }
     #
-    generators = "CMakeDeps"
-    exports_sources = "src/*", "CMakeLists.txt", "Findjwt-cpp.cmake"
+    exports_sources = "src/*"
     no_copy_source = True
     build_policy = "missing"
-    #
-    _openssl_version = "[>=3.0.13]"
-    _openssl_channel = "stable" 
 
-#    def layout(self):
-#        basic_layout(self, src_folder="src")
+    def layout(self):
+        tools.cmake.cmake_layout(self, src_folder="src")
 
     def requirements(self):
-        self.requires("openssl/%s@%s/%s" % (self._openssl_version, self.user, self._openssl_channel))
+        self.requires("openssl/[>=3.0.16]@%s/stable" % self.user)
         if self.options.with_picojson:
             self.requires("picojson/1.3.0")
-
-    def package_id(self):
-        self.info.header_only()
+            
+    def generate(self):
+        benv = tools.env.VirtualBuildEnv(self)
+        benv.generate()
+        renv = tools.env.VirtualRunEnv(self)
+        renv.generate()
+        deps = tools.cmake.CMakeDeps(self)    
+        deps.generate()
+        tc = tools.cmake.CMakeToolchain(self)
+        tc.variables["JWT_BUILD_EXAMPLES"] = "OFF"
+        tc.generate()
+        
+    def build(self):
+        cmake = tools.cmake.CMake(self)
+        cmake.configure()
+        cmake.build()
 
     def package(self):
-        source_dir = os.path.join(self.source_folder, "src")
-        header_dir = os.path.join(source_dir, "include", "jwt-cpp")
-        copy(self, pattern="*.h", dst=os.path.join(self.package_folder, "include", "jwt-cpp"), src=header_dir, keep_path=True)
-        copy(self, "LICENSE", dst=os.path.join(self.package_folder,"licenses"), src=source_dir)
-        copy(self, "Findjwt-cpp.cmake", dst=self.package_folder, src=self.source_folder)
+        cmake = tools.cmake.CMake(self)
+        cmake.install()
+        tools.files.rmdir(self, os.path.join(self.package_folder, "cmake"))
+
+    def package_id(self):
+        self.info.clear()
 
     def package_info(self):
+        self.cpp_info.set_property("cmake_find_mode", "both")
         self.cpp_info.bindirs = []
         self.cpp_info.libdirs = []
         self.cpp_info.requires = ["openssl::openssl"]
-        if self.options.with_picojson:
-            self.cpp_info.requires.append("picojson::picojson")
-        else:
+        if not self.options.with_picojson:
             self.cpp_info.defines.append("JWT_DISABLE_PICOJSON")
 
